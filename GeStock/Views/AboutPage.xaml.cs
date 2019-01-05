f797d31f4a59ffde5df5005119fa813c1652f396
Elvira Tagny <elvira.tagny@soprasteria.com>
2019-01-05 07:24:00 +0100

    first commit

---
+++ GeStock/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GeStock
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GeStock/Views/AuthenticationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GeStock.Views
+{
+    public partial class AuthenticationPage : ContentPage
+    {
+        public AuthenticationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
